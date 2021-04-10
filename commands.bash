2b248eb438234a0551f124a204d2f66252eaace1
GeoSn0w <geosn0w@fce365.info>
2021-04-10 21:04:38 +0300

    Update commands.bash

--- commands.bash
+++ commands.bash
@@ -7,3 +7,6 @@ dpkg -b Folder
 # Create the iOS Repo Packages archive for Cydia, etc.
 gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
+
+# Kill the darn usbd and stop iPhone from indefinitely connecting and disconnecting fast (Fuck you too, macOS Catalina :/)
+sudo killall -STOP usbd
